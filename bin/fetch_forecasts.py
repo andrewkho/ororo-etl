73b830aac73fc55e3358f83567fa7d6f1dddce2c
Andrew Ho <andrew.ho@stitchfix.com>
2018-04-08 23:40:15 -0700

    Revert "Setup logger for file + stdout"
    This reverts commit 3c86623eb277b8765407370ba5057be8b683ac1d.

--- bin/fetch_forecasts.py
+++ bin/fetch_forecasts.py
@@ -5,7 +5,7 @@ from logging.handlers import RotatingFileHandler
 from extract.yahoo import YahooExtractor
 
 LOG_FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-LOG_FILENAME = 'log/fetch_forecasts.log'
+LOG_FILENAME = '/var/log/fetch_forecasts.log'
 LOG_MAXBYTES = 20 * 1024 * 1024  # 20 megabytes
 LOG_BACKUPCOUNT = 5  # Hold at most 100 megabytes of log file
 LOG_LEVEL = logging.INFO  # Only record INFO level or higher in log
