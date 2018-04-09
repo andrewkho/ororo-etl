87816efc9a9a068629ac7c67c83a9b0f499f75f4
Andrew Ho <andrew.ho@stitchfix.com>
2018-04-08 23:22:05 -0700

    Setup logger for file + stdout

--- bin/fetch_forecasts.py
+++ bin/fetch_forecasts.py
@@ -1,12 +1,29 @@
 import logging
 import sys
+from logging.handlers import RotatingFileHandler
 
 from extract.yahoo import YahooExtractor
 
-log_format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-logging.basicConfig(stream=sys.stdout, level=logging.DEBUG, format=log_format)
+LOG_FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+LOG_FILENAME = '/var/log/fetch_forecasts.log'
+LOG_MAXBYTES = 20 * 1024 * 1024  # 20 megabytes
+LOG_BACKUPCOUNT = 5  # Hold at most 100 megabytes of log file
+LOG_LEVEL = logging.INFO  # Only record INFO level or higher in log
 
-logger = logging.getLogger(__name__)
+logger = logging.getLogger('extract')
+
+logger.setLevel(logging.DEBUG)
+filehandler = RotatingFileHandler(filename=LOG_FILENAME, maxBytes=LOG_MAXBYTES, backupCount=LOG_BACKUPCOUNT)
+filehandler.setLevel(LOG_LEVEL)
+formatter = logging.Formatter(LOG_FORMAT)
+filehandler.setFormatter(formatter)
+logger.addHandler(filehandler)
+
+stdouthandler = logging.StreamHandler(sys.stdout)
+stdouthandler.setLevel(logging.DEBUG)
+formatter = logging.Formatter(LOG_FORMAT)
+stdouthandler.setFormatter(formatter)
+logger.addHandler(stdouthandler)
 
 
 def fetch_forecasts():
